3ea2f95181eebab594c7f2d0069903b94d4b6915
Søren Elmholt <soren@elmholt.com>
2025-03-15 18:11:52 +0100

    test

--- menu.ps1
+++ menu.ps1
@@ -3,12 +3,12 @@ function Show-Menu {
           [string]$Title = 'My Menu'
      )
      Clear-Host
-     Write-Host "================ $Title ================"
+     Write-Host " ================ $Title ================ "
     
-     Write-Host "1: Press '1' for this option."
-     Write-Host "2: Press '2' for this option."
-     Write-Host "3: Press '3' for this option."
-     Write-Host "Q: Press 'Q' to quit."
+     Write-Host " 1: Press '1' for this option. "
+     Write-Host " 2: Press '2' for this option. "
+     Write-Host " 3: Press '3' for this option. "
+     Write-Host " Q: Press 'Q' to quit. "
 }
 
 Set-ExecutionPolicy Bypass -Scope Process
