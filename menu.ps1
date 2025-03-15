fcdc51de7fa4a9210aa20519ebc2040f649bf98e
Søren Elmholt <soren@elmholt.com>
2025-03-15 18:10:53 +0100

    test

--- menu.ps1
+++ menu.ps1
@@ -3,24 +3,25 @@ function Show-Menu {
           [string]$Title = 'My Menu'
      )
      Clear-Host
-     Write-Host “================ $Title ================”
+     Write-Host "================ $Title ================"
     
-     Write-Host “1: Press '1' for this option.”
-     Write-Host “2: Press '2' for this option.”
-     Write-Host “3: Press '3' for this option.”
-     Write-Host “Q: Press 'Q' to quit.”
+     Write-Host "1: Press '1' for this option."
+     Write-Host "2: Press '2' for this option."
+     Write-Host "3: Press '3' for this option."
+     Write-Host "Q: Press 'Q' to quit."
 }
 
 Set-ExecutionPolicy Bypass -Scope Process
+$WorkingDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 
 do {
      Show-Menu
-     $input = Read-Host “Please make a selection”
+     $input = Read-Host "Please make a selection"
      switch ($input) {
           '1' {
                Clear-Host
                'You chose option #1'
-               Write-host "$psscriptroot"
+               Write-host "$WorkingDir"
           } '2' {
                Clear-Host
                'You chose option #2'
