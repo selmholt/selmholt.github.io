d36b369ca3aec60911433b4554e2ca075ea8368a
Søren Elmholt <soren@elmholt.com>
2025-03-15 18:01:50 +0100

    Updated menu.ps1

--- menu.ps1
+++ menu.ps1
@@ -1,37 +1,34 @@
-function Show-Menu
- {
-            param (
-            [string]$Title = 'My Menu'
-            )
-            cls
-            Write-Host “================ $Title ================”
+function Show-Menu {
+     param (
+          [string]$Title = 'My Menu'
+     )
+     Clear-Host
+     Write-Host “================ $Title ================”
     
-            Write-Host “1: Press '1' for this option.”
-            Write-Host “2: Press '2' for this option.”
-            Write-Host “3: Press '3' for this option.”
-            Write-Host “Q: Press 'Q' to quit.”
- }
+     Write-Host “1: Press '1' for this option.”
+     Write-Host “2: Press '2' for this option.”
+     Write-Host “3: Press '3' for this option.”
+     Write-Host “Q: Press 'Q' to quit.”
+}
 
 
- do
- {
-            Show-Menu
-            $input = Read-Host “Please make a selection”
-            switch ($input)
-            {
-            '1' {
-                 cls
-                 'You chose option #1'
-            } '2' {
-                 cls
-                 'You chose option #2'
-            } '3' {
-                 cls
-                 'You chose option #3'
-            } 'q' {
-                 return
-            }
-            }
-            pause
- }
- until ($input -eq 'q')
+do {
+     Show-Menu
+     $input = Read-Host “Please make a selection”
+     switch ($input) {
+          '1' {
+               Clear-Host
+               'You chose option #1'
+          } '2' {
+               Clear-Host
+               'You chose option #2'
+          } '3' {
+               Clear-Host
+               'You chose option #3'
+          } 'q' {
+               return
+          }
+     }
+     pause
+}
+until ($input -eq 'q')
